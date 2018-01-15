175f711fda37ed26195282bd0373fa8608a1875a
e2zhaojing <zhaojing@e2info.cn>
2018-01-15 15:35:53 +0800

    The wp-botman's development.

--- wp-botman.php
+++ wp-botman.php
@@ -33,12 +33,60 @@ define("WPBOTMAN_PLUGIN_DIR", __file__);
 define("WPBOTMAN_PLUGIN_BASE", dirname(__file__));
 define("WPBOTMAN_PLUGIN_URL", plugin_dir_url(WPBOTMAN_PLUGIN_DIR));
 
+
+
 define("WPBOTMAN_PLUGIN_JS_DIR", WPBOTMAN_PLUGIN_URL."assets/js/");
 define("WPBOTMAN_PLUGIN_CSS_DIR", WPBOTMAN_PLUGIN_URL . "assets/css/");
-define("WPBOTMAN_PLUGIN_INCLUDE_DIR", WPBOTMAN_PLUGIN_BASE . "/include/");
+define("WPBOTMAN_PLUGIN_INCLUDE_DIR", WPBOTMAN_PLUGIN_BASE . "/includes/");
 
-include_once(WPBOTMAN_PLUGIN_INCLUDE_DIR . '/functions.php');
+//include_once(WPBOTMAN_PLUGIN_INCLUDE_DIR . '/functions.php');
+//include_once(WPBOTMAN_PLUGIN_INCLUDE_DIR . '/admin/settings/register-settings.php');
+include_once(WPBOTMAN_PLUGIN_INCLUDE_DIR . '/actions.php');
+include_once(WPBOTMAN_PLUGIN_INCLUDE_DIR . '/template-functions.php');
+include_once(WPBOTMAN_PLUGIN_INCLUDE_DIR . '/shortcodes.php');
+include_once(WPBOTMAN_PLUGIN_INCLUDE_DIR . '/widgets.php');
+include_once(WPBOTMAN_PLUGIN_INCLUDE_DIR . '/scripts.php');
+wp_register_script('adminjs', WPBOTMAN_PLUGIN_JS_DIR . 'admin.js', array('jquery')); //put any dependencies (including jQuery) into the array
+wp_enqueue_script('adminjs');
+//include_once(WPBOTMAN_PLUGIN_INCLUDE_DIR . '/post-meta-box.php');
 
-add_action('init', 'wpbotman_init');
+
+    //require_once WPBOTMAN_PLUGIN_INCLUDE_DIR . '/admin/admin-actions.php';
+    require_once WPBOTMAN_PLUGIN_INCLUDE_DIR . '/admin/admin-pages.php';
+    //require_once WPBOTMAN_PLUGIN_INCLUDE_DIR . '/admin/settings/display-settings.php';
+    //require_once WPBOTMAN_PLUGIN_INCLUDE_DIR . '/admin/upgrades/upgrade-functions.php';
+    //require_once WPBOTMAN_PLUGIN_INCLUDE_DIR . '/admin/welcome.php';
+
+
+
+
+
+
+/*function myc_is_func_disabled( $function ) {
+    $disabled = explode( ',',  ini_get( 'disable_functions' ) );
+
+    return in_array( $function, $disabled );
+}*/
+
+
+
+
+
+
+
+
+
+//do_action('[wp_footer]');
+/*add_action('init', 'wpbotman_init');
 add_action('wp_enqueue_scripts', 'wpbotman_scripts');
 add_action('admin_menu', 'wpbotman_admin_menu');
+add_action('init', 'wpbotman_shortcodes');*/
+
+/*wp_register_style('frontendcss', WPBOTMAN_PLUGIN_CSS_DIR . 'frontend.css', array(), '1.0.0', "all"); //put any dependencies (including jQuery) into the array
+wp_enqueue_style('frontendcss');*/
+
+/*wp_register_script('frontendjs', WPBOTMAN_PLUGIN_JS_DIR . 'frontend.js', array('jquery')); //put any dependencies (including jQuery) into the array
+wp_enqueue_script('frontendjs');*/
+
+
+?>
