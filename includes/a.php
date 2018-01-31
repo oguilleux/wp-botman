ce15d8d6a708ba32670813f06de7d2a0574999b2
e2zhaojing <zhaojing@e2info.cn>
2018-01-31 16:14:01 +0800

    delete a.php

--- includes/a.php
+++
@@ -1,7 +0,0 @@
-<form action="/wp-admin/admin-ajax.php" method="post">
-    <input type="text" name="action" value="bm_send">
-    <input type="text" name="driver" value="web">
-    <input type="text" name="userId" value="aaaaaa">
-    <input type="text" name="message" value="hello">
-    <input type="submit" name="aaa" value="bbb">
-</form>
